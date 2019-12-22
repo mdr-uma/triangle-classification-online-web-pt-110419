0e6735936e850cbb690453719f9b8abd182a70c4
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-22 18:37:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3, :kind
+
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    self.
+  end
 end
