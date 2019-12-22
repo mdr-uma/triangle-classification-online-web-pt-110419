c4531440f4da279abbc98ab9d286ce35cf072d8a
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-22 19:02:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,14 +6,13 @@ class Triangle
     @side_3 = size_3
   end
 
-  def kind
+  def kind()
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
-    elsif (@side_1 + @side_2 <= @side_3) ||  (@side_1 + @side_3 <= @side_2) || (@side_2 + @side_3 <= @side_1)
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
-      else
-        if
-         (@side_1 == @side_2) && (@side_2 == @side_3)
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
       elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
         :isosceles
@@ -21,8 +20,11 @@ class Triangle
         :scalene
       end
     end
+
   end
+
 end
 
-  class TriangleError < StandardError
-  end
+class TriangleError < StandardError
+  # triangle error code
+end
