26062ab5948102a6fb382039784f6ecd90e9c393
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-22 19:07:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,8 +11,8 @@ class Triangle
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
-    else
-      if (@side_1 == @side_2) && (@side_2 == @side_3)
+    elsif
+       (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
       elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
         :isosceles
