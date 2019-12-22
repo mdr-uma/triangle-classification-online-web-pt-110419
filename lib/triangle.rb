f27d48e5e7fc56dce5d6407b1fe15b8e5a2fc0d6
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-22 18:48:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   end
 
   def kind
-    if (size_1 >= 0) == (size_2 >= 0) && (size_2 >= 0) == (size_3 >= 0) && (size_1 >= 0) == (size_3 >=)
+    if (size_1 >= 0) == (size_2 >= 0) && (size_2 >= 0) == (size_3 >= 0) && (size_1 >= 0) == (size_3 >=0)
       :equilateral
 
     end
